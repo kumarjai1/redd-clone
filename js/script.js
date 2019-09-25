ee82143b03b9c2d04cfd908ba3d9343448049a27
kumarjai1 <jaiessarani@gmail.com>
2019-09-25 12:03:21 -0400

    display all posts

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const allPosts = document.getElementById('allPosts');
 
+//fetches the post API
 function fetchAPI () {
     fetch(`http://thesi.generalassemb.ly:8080/post/list`)
     .then(response => response.json())
@@ -10,17 +11,29 @@ function fetchAPI () {
     .catch(err => console.log(err)); 
 }
 
+//displays all posts
 function displayPosts(arr) {
     for (let i = 0; i <= 50; i++) {
+
+      //creates tags for the post elements in the frontend
       let postContainer = document.createElement('div');
       let postOwner = document.createElement('h4');
-    
+      let postTitle = document.createElement('h1');
+      let postContent = document.createElement('p');
+
+      //defines the content to the tags
       postContainer.classList.add('container');
       postOwner.innerText = arr[i].user.username;
+      postTitle.innerText = arr[i].title;
+      postContent.innerText = arr[i].description;
 
-      console.log(postOwner);
+      console.log(postContent.innerText);
+
+      //adds to the post container
       allPosts.append(postContainer);
       postContainer.append(postOwner);
+      postContainer.append(postTitle);
+      postContainer.append(postContent);
     }
 }
 fetchAPI();
