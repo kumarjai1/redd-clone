d320fb30d36497de6fb98ca460f45a159566402a
kumarjai1 <jaiessarani@gmail.com>
2019-09-25 11:53:42 -0400

    display fetch results frontend

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const allPosts = document.getElementById('allPosts');
 
 function fetchAPI () {
     fetch(`http://thesi.generalassemb.ly:8080/post/list`)
@@ -10,6 +11,16 @@ function fetchAPI () {
 }
 
 function displayPosts(arr) {
+    for (let i = 0; i <= 50; i++) {
+      let postContainer = document.createElement('div');
+      let postOwner = document.createElement('h4');
     
+      postContainer.classList.add('container');
+      postOwner.innerText = arr[i].user.username;
+
+      console.log(postOwner);
+      allPosts.append(postContainer);
+      postContainer.append(postOwner);
+    }
 }
 fetchAPI();
