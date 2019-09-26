5ed2b97f47de85c6f30f1475a0bce15da9857c72
Steven Huang <steven.charles.huang@gmail.com>
2019-09-26 10:57:53 -0400

    fixed single page click handler bug

--- js/script.js
+++ js/script.js
@@ -47,29 +47,37 @@ function displayPosts(arr) {
       postTitle.addEventListener('click', function(e) {
         console.log(e.target.dataset.id);
         currentPostID = e.target.dataset.id;
+
+        // Empties All Posts View for Single Post View
         allPosts.innerHTML = '';
+        postContainer.innerHTML = '';
 
         postsArr.forEach(post => {
-          if (post.id === currentPostID) {
-            console.log(postsArr);
+          if (post.id === parseInt(currentPostID)) {
+
             currentPost.id = post.id;
             currentPost.title = post.title;
             currentPost.description = post.description;
             currentPost.user = post.user;
-            console.log({currentPost});
+            console.log('currentPost', currentPostID);
+
+            // Create Single View Post Elements
+            const singlePostTitle = document.createElement('h1');
 
             postContainer.classList.add('container');
             postOwner.innerText = 'posted by ' + post.user.username;
-            postTitle.innerText = post.title;
+            singlePostTitle.innerText = post.title;
             postContent.innerText = post.description;
+
+            // Append Single View Post Elements
+            postContainer.append(postOwner);
+            postContainer.append(singlePostTitle);
+            postContainer.append(postContent);
+            allPosts.append(postContainer);
+            console.log(postContainer);
           }
         });
 
-
-        allPosts.append(postContainer);
-        postContainer.append(postOwner);
-        postContainer.append(postTitle);
-        postContainer.append(postContent);
       })
 
       postStyling (postOwner, postTitle, postContent);
