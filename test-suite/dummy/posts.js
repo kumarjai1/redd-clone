eea4699de4b7e07180d4ea21eabe0efa3b3dbf5b
Steven Huang <steven.charles.huang@gmail.com>
2019-09-28 22:37:51 -0400

    moved chai-dom to libs, added dummy data and dummy dom elements, added displayPosts() tests

---
+++ test-suite/dummy/posts.js
@@ -0,0 +1,234 @@
+let dummyPosts = [
+  {
+      "id": 1691,
+      "title": "I like water!!",
+      "description": "I am strong",
+      "user": {
+          "username": "aquaman"
+      }
+  },
+  {
+      "id": 1692,
+      "title": "FIIIIIIIIRST",
+      "description": "Faster than the Flash",
+      "user": {
+          "username": "wonderwoman2"
+      }
+  },
+  {
+      "id": 1693,
+      "title": "qctest",
+      "description": "qctest",
+      "user": {
+          "username": "venom"
+      }
+  },
+  {
+      "id": 1694,
+      "title": "I am super cool!!",
+      "description": "I think it's my dress that makes me so cool.",
+      "user": {
+          "username": "venom"
+      }
+  },
+  {
+      "id": 1695,
+      "title": "kljafldkajsd",
+      "description": "skljfdlkajsdf\n",
+      "user": {
+          "username": "adfasfasd"
+      }
+  },
+  {
+      "id": 1696,
+      "title": "I am super cool BatmanRobin!!",
+      "description": "I think it's my dress that makes me so cool.",
+      "user": {
+          "username": "batmanrobin1"
+      }
+  },
+  {
+      "id": 1697,
+      "title": "test123",
+      "description": "test",
+      "user": {
+          "username": "theflash"
+      }
+  },
+  {
+      "id": 1698,
+      "title": "Delete me",
+      "description": "Delete delete delete",
+      "user": {
+          "username": "fake"
+      }
+  },
+  {
+      "id": 1699,
+      "title": "I love cheeseburgers",
+      "description": "My suit can be tough to get into.",
+      "user": {
+          "username": "ironman"
+      }
+  },
+  {
+      "id": 1700,
+      "title": "luigi arrives",
+      "description": "its meee, luiiigiii",
+      "user": {
+          "username": "luigi"
+      }
+  },
+  {
+      "id": 1701,
+      "title": "luigi arrives",
+      "description": "its meee, luiiigiii",
+      "user": {
+          "username": "luigi"
+      }
+  },
+  {
+      "id": 1702,
+      "title": "perfume",
+      "description": "too much",
+      "user": {
+          "username": "paris"
+      }
+  },
+  {
+      "id": 1703,
+      "title": "Bottle",
+      "description": "Give me one",
+      "user": {
+          "username": "paris"
+      }
+  },
+  {
+      "id": 1704,
+      "title": "qctest2title",
+      "description": "qctest2content",
+      "user": {
+          "username": "venom"
+      }
+  },
+  {
+      "id": 1705,
+      "title": "qctest3title",
+      "description": "qctest3content",
+      "user": {
+          "username": "venom"
+      }
+  },
+  {
+      "id": 1706,
+      "title": "qctest4title",
+      "description": "qctest4content",
+      "user": {
+          "username": "venom"
+      }
+  },
+  {
+      "id": 1707,
+      "title": "dada",
+      "description": "dada",
+      "user": {
+          "username": "paris"
+      }
+  },
+  {
+      "id": 1708,
+      "title": "dada",
+      "description": "dada",
+      "user": {
+          "username": "paris"
+      }
+  },
+  {
+      "id": 1709,
+      "title": "dada",
+      "description": "dada",
+      "user": {
+          "username": "paris"
+      }
+  },
+  {
+      "id": 1710,
+      "title": "dada",
+      "description": "dada",
+      "user": {
+          "username": "paris"
+      }
+  },
+  {
+      "id": 1711,
+      "title": "qctest5title",
+      "description": "qctest5content",
+      "user": {
+          "username": "venom"
+      }
+  },
+  {
+      "id": 1712,
+      "title": "qctest6title",
+      "description": "qctest6content\n",
+      "user": {
+          "username": "venom"
+      }
+  },
+  {
+      "id": 1713,
+      "title": "pink",
+      "description": "panther",
+      "user": {
+          "username": "luigi"
+      }
+  },
+  {
+      "id": 1714,
+      "title": "qctest7title",
+      "description": "qctest7content",
+      "user": {
+          "username": "venom"
+      }
+  },
+  {
+      "id": 1715,
+      "title": "qctest8title",
+      "description": "qctest8content",
+      "user": {
+          "username": "venom"
+      }
+  },
+  {
+      "id": 1716,
+      "title": "qctest9title",
+      "description": "qctest9content",
+      "user": {
+          "username": "venom"
+      }
+  },
+  {
+      "id": 1717,
+      "title": "qctest10title",
+      "description": "qctest10content",
+      "user": {
+          "username": "venom"
+      }
+  },
+  {
+      "id": 1718,
+      "title": "qctest11title",
+      "description": "qctest11content",
+      "user": {
+          "username": "venom"
+      }
+  },
+  {
+      "id": 1719,
+      "title": "test",
+      "description": "test body",
+      "user": {
+          "username": "CookieMonster"
+      }
+  },
+];
